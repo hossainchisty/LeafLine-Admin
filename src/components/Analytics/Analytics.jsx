1a548c2a43631d89f882dd82fdb299015dac0d7d
hossainchisty <hossain.chisty11@gmail.com>
2023-09-17 13:01:27 +0600

    feat: Added dynamic properties

--- src/components/Analytics/Analytics.jsx
+++ src/components/Analytics/Analytics.jsx
@@ -1,36 +1,104 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useState, useEffect, useMemo } from "react";
+import ClipLoader from "react-spinners/ClipLoader";
+
 const Analytics = () => {
-  const totalBooks = 1000;
-  const totalUsers = 500;
-  const featuredBooks = 250;
-  return (
-    <>
-      <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-        {/* Analytics Card 1: Total Books */}
-        <div className="bg-white shadow-lg rounded-lg p-6">
-          <h3 className="text-lg font-semibold text-gray-800 mb-2">
-            Total Books
-          </h3>
-          <p className="text-3xl font-bold text-blue-500">{totalBooks}</p>
-        </div>
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [statistics, setStatistics] = useState({
+    totalBooks: 0,
+    totalUsers: 0,
+    featuredBooks: 0,
+  });
+  const apiBaseDomain = import.meta.env.VITE_API_BASE_URL;
 
-        {/* Analytics Card 2: Total Users */}
-        <div className="bg-white shadow-lg rounded-lg p-6">
-          <h3 className="text-lg font-semibold text-gray-800 mb-2">
-            Total Users
-          </h3>
-          <p className="text-3xl font-bold text-green-500">{totalUsers}</p>
-        </div>
+  useEffect(() => {
+    fetch(`${apiBaseDomain}/analytics`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setStatistics({
+          totalBooks: data.data.total_books,
+          totalUsers: data.data.total_users,
+          featuredBooks: data.data.featured_books,
+        });
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setIsLoading(false);
+      });
+  }, []);
 
-        {/* Analytics Card 3: Featured Books */}
-        <div className="bg-white shadow-lg rounded-lg p-6">
-          <h3 className="text-lg font-semibold text-gray-800 mb-2">
-            Featured Books
-          </h3>
-          <p className="text-3xl font-bold text-yellow-500">{featuredBooks}</p>
+  const analyticsCards = useMemo(() => {
+    return (
+      <>
+        <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+          {/* Analytics Card 1: Total Books */}
+          <div className="bg-white shadow-lg rounded-lg p-6">
+            <h3 className="text-lg font-semibold text-gray-800 mb-2">
+              Total Books
+            </h3>
+            <p className="text-3xl font-bold text-blue-500">
+              {statistics.totalBooks}
+            </p>
+          </div>
+
+          {/* Analytics Card 2: Total Users */}
+          <div className="bg-white shadow-lg rounded-lg p-6">
+            <h3 className="text-lg font-semibold text-gray-800 mb-2">
+              Total Users
+            </h3>
+            <p className="text-3xl font-bold text-green-500">
+              {statistics.totalUsers}
+            </p>
+          </div>
+
+          {/* Analytics Card 3: Featured Books */}
+          <div className="bg-white shadow-lg rounded-lg p-6">
+            <h3 className="text-lg font-semibold text-gray-800 mb-2">
+              Featured Books
+            </h3>
+            <p className="text-3xl font-bold text-yellow-500">
+              {statistics.featuredBooks}
+            </p>
+          </div>
         </div>
+      </>
+    );
+  }, [statistics]);
+
+  const override = {
+    display: "block",
+    margin: "0 auto",
+    borderColor: "rgba(106, 89, 187, 0.888)",
+  };
+
+  if (isLoading) {
+    return (
+      <div>
+        {" "}
+        <ClipLoader
+          color={"rgba(106, 89, 187, 0.888)"}
+          loading={isLoading}
+          cssOverride={override}
+          size={120}
+          aria-label="Loading Spinner"
+          data-testid="loader"
+        />
       </div>
-    </>
-  );
+    );
+  }
+
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
+
+  return analyticsCards;
 };
 
 export default Analytics;
--- src/pages/Admin/AdminUsers.jsx
+++ src/pages/Admin/AdminUsers.jsx
@@ -1,20 +1,59 @@
 import { useState, useEffect } from "react";
 import { formatISO9075 } from "date-fns";
+import ClipLoader from "react-spinners/ClipLoader";
 
 function AdminUsers() {
   const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const apiBaseDomain = import.meta.env.VITE_API_BASE_URL;
 
   useEffect(() => {
     // Fetch user data and set it to the users state
-    fetch("http://localhost:8000/api/v1/users/list")
-      .then((response) => response.json())
+    fetch(`${apiBaseDomain}/users/`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
       .then((data) => {
-        setUsers(data);
+        // Flatten the nested array structure
+        const flattenedUsers = data.data.flat();
+        setUsers(flattenedUsers);
+        setLoading(false);
       })
       .catch((error) => {
-        console.error("Error fetching user data:", error);
+        setError(error);
+        setLoading(false);
       });
-  }, []);
+  }, [apiBaseDomain]);
+
+  const override = {
+    display: "block",
+    margin: "0 auto",
+    borderColor: "rgba(106, 89, 187, 0.888)",
+  };
+
+  if (loading) {
+    return (
+      <div>
+        {" "}
+        <ClipLoader
+          color={"rgba(106, 89, 187, 0.888)"}
+          loading={loading}
+          cssOverride={override}
+          size={120}
+          aria-label="Loading Spinner"
+          data-testid="loader"
+        />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
 
   return (
     <div className="flex flex-col m-5 pt-5">
@@ -45,7 +84,7 @@ function AdminUsers() {
                       {user.email}
                     </td>
                     <td className="whitespace-nowrap px-6 py-4">
-                    {formatISO9075(new Date(user.createdAt))}
+                      {formatISO9075(new Date(user.createdAt))}
                     </td>
                   </tr>
                 ))}
