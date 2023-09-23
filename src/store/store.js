64fa2a253dbae9a840bb6be8c45fc4e2931943d9
hossainchisty <hossainchisty@users.noreply.github.com>
2023-09-23 13:38:58 +0000

    style: format files

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,5 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { userApi } from '../services/userSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import { userApi } from "../services/userSlice";
 
 export const store = configureStore({
   reducer: {
